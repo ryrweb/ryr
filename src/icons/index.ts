2f3350549da8e29235e427061529dd522ffe9e72
Martín Vega <mvegaxx@gmail.com>
2022-06-16 22:45:04 -0500

    feat: add icons folder

---
+++ src/icons/index.ts
@@ -0,0 +1,19 @@
+import mase from "../icons/mase.png";
+import license from "../icons/driver-license.png";
+import portfolio from "../icons/portfolio.png";
+import success from "../icons/success.png";
+import warning from "../icons/warning.png";
+import copy from "../icons/copy.png";
+import shield from "../icons/shield.png";
+import museum from "../icons/museum.png";
+
+export default {
+  mase,
+  license,
+  portfolio,
+  success,
+  warning,
+  copy,
+  shield,
+  museum,
+};
