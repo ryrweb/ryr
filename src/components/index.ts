339cd9ff8c5a564dbca54e70bceb78b330c85bb7
Martín Vega <mvegaxx@gmail.com>
2022-07-09 22:02:45 -0500

    feat: add index to get components

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar.astro";
+import Slider from "./Slider.astro";
+import Values from "./Values.astro";
+import About from "./About.astro";
+import Areas from "./Areas.astro";
+import Services from "./Services.astro";
+import Testimonials from "./Testimonials.astro";
+import Partners from "./Partners.astro";
+import Contact from "./Contact.astro";
+
+export default {
+  Navbar,
+  Slider,
+  Values,
+  About,
+  Areas,
+  Services,
+  Testimonials,
+  Partners,
+  Contact,
+};
